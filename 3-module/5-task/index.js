5ece2848ced31f7367317989a6b69d99ce9bde4b
2rchinsky <denisturukin@mail.ru>
2023-04-04 20:47:01 +0400

    задачи функции и массивы

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,8 +1,7 @@
 function getMinMax(str) {
   let arr = str
     .split(' ')
-    .map(item => +item)
-    .filter(item => !Number.isNaN(item));
+    .filter(item => !Number.isNaN(+item));
   return {
     min: Math.min(...arr),
     max: Math.max(...arr),
